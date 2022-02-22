53db80d4ac6502db0a6b8bb877000e8bbf750616
Luiz Fernando Ribeiro <luizf_ribeiro@outlook.com>
2022-02-22 17:41:13 -0300

    adicionando conteudo e player para vizualização dos trailers

---
+++ js/owl/popup.js
@@ -0,0 +1,93 @@
+function acao(){
+
+    let modal = document.querySelector('.modal')
+
+
+    modal.style.display = 'block';
+}
+
+
+function fechar(){
+
+    let modal = document.querySelector('.modal')
+
+
+    modal.style.display = 'none';
+
+}
+
+
+function acao1(){
+
+    let modal = document.querySelector('.modal1')
+
+
+    modal.style.display = 'block';
+}
+
+
+
+function acao2(){
+
+    let modal = document.querySelector('.modal2')
+
+
+    modal.style.display = 'block';
+}
+
+function acao3(){
+
+    let modal = document.querySelector('.modal3')
+
+
+    modal.style.display = 'block';
+}
+
+
+function acao4(){
+
+    let modal = document.querySelector('.modal4')
+
+
+    modal.style.display = 'block';
+}
+function acao5(){
+
+    let modal = document.querySelector('.modal5')
+
+
+    modal.style.display = 'block';
+}
+
+
+function acao6(){
+
+    let modal = document.querySelector('.modal6')
+
+
+    modal.style.display = 'block';
+}
+
+function acao7(){
+
+    let modal = document.querySelector('.modal7')
+
+
+    modal.style.display = 'block';
+}
+
+function acao8(){
+
+    let modal = document.querySelector('.modal8')
+
+
+    modal.style.display = 'block';
+}
+
+function acao9(){
+
+    let modal = document.querySelector('.modal9')
+
+
+    modal.style.display = 'block';
+}

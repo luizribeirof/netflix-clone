eba2ef238a28e65d7406f60f06be3b6aba611d12
Luiz Fernando Ribeiro <luizf_ribeiro@outlook.com>
2022-02-23 14:14:45 -0300

    adicionado links para vizualizar os videos

--- js/owl/popup.js
+++ js/owl/popup.js
@@ -91,3 +91,152 @@ function acao9(){
 
     modal.style.display = 'block';
 }
+
+
+function acao10(){
+
+    let modal = document.querySelector('.modal10')
+
+
+    modal.style.display = 'block';
+}
+function acao11(){
+
+    let modal = document.querySelector('.modal11')
+
+
+    modal.style.display = 'block';
+}
+function acao12(){
+
+    let modal = document.querySelector('.modal12')
+
+
+    modal.style.display = 'block';
+}
+function acao13(){
+
+    let modal = document.querySelector('.modal13')
+
+
+    modal.style.display = 'block';
+}
+function acao14(){
+
+    let modal = document.querySelector('.modal14')
+
+
+    modal.style.display = 'block';
+}
+function acao15(){
+
+    let modal = document.querySelector('.modal15')
+
+
+    modal.style.display = 'block';
+}
+function acao16(){
+
+    let modal = document.querySelector('.modal16')
+
+
+    modal.style.display = 'block';
+}
+function acao17(){
+
+    let modal = document.querySelector('.modal17')
+
+
+    modal.style.display = 'block';
+}
+function acao18(){
+
+    let modal = document.querySelector('.modal18')
+
+
+    modal.style.display = 'block';
+}
+function acao19(){
+
+    let modal = document.querySelector('.modal19')
+
+
+    modal.style.display = 'block';
+}
+function acao20(){
+
+    let modal = document.querySelector('.modal20')
+
+
+    modal.style.display = 'block';
+}
+function acao21(){
+
+    let modal = document.querySelector('.modal21')
+
+
+    modal.style.display = 'block';
+}
+function acao22(){
+
+    let modal = document.querySelector('.modal22')
+
+
+    modal.style.display = 'block';
+}
+function acao23(){
+
+    let modal = document.querySelector('.modal23')
+
+
+    modal.style.display = 'block';
+}
+function acao24(){
+
+    let modal = document.querySelector('.modal24')
+
+
+    modal.style.display = 'block';
+}
+function acao25(){
+
+    let modal = document.querySelector('.modal25')
+
+
+    modal.style.display = 'block';
+}
+function acao26(){
+
+    let modal = document.querySelector('.modal26')
+
+
+    modal.style.display = 'block';
+}
+function acao27(){
+
+    let modal = document.querySelector('.modal27')
+
+
+    modal.style.display = 'block';
+}
+function acao28(){
+
+    let modal = document.querySelector('.modal28')
+
+
+    modal.style.display = 'block';
+}
+function acao29(){
+
+    let modal = document.querySelector('.modal29')
+
+
+    modal.style.display = 'block';
+}
+function acao30(){
+
+    let modal = document.querySelector('.modal30')
+
+
+    modal.style.display = 'block';
+}
